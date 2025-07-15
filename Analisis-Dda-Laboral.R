3bbe827d0512aa8354cc3135a6f7943b4c2ca703
luis-fernandezt <luisguillermo.fernandez@outlook.com>
2025-07-15 12:48:22 -0400

    Estimación del número de empresas y trabajadores por región, distribución porcentual (%) y tamaño muestral.

--- Analisis-Dda-Laboral.R
+++ Analisis-Dda-Laboral.R
@@ -10,6 +10,7 @@ library(tidyverse)
 
 library(survey)
 library(srvyr)
+library(car)
 
 # Descarga de Bases de datos
 dir.create("bd")
@@ -26,33 +27,79 @@ download.file("https://www.subtrab.gob.cl/wp-content/uploads/2025/05/Libro_de_co
 # Cálculos
 ENADEL_2024 <- read_csv("bd/ENADEL_2024_externa.csv")
 
-ENADEL_2024 %>% #el n muestral = 5965
-  select(a2) %>%
-  summarise(count = n())
+# Factor de expasion a la muestra
+# Según el documento metodológico de ENADEL 2024, el diseño muestral es probabilístico y estratificado, con estratos definidos por las 16 regiones y 12 agrupaciones de ramas de actividad económica, pero no se menciona un nivel de conglomerado específico (clusters). Por tanto, no hay una variable conglomerado oficial en los datos.
 
-# Factor de expasi+on a la muestra
-svydsgn <- ENADEL_2024 %>% as_survey_design(weights = fact_emp, 
+svydsgn <- ENADEL_2024 %>% as_survey_design(ids = 1,
+                                            weights = fact_emp, 
                                             strata = estrato)
-                                            #ids = id_directorio) # no viene  dado
 
 #total de trabajadores
-svytotal(~b6_1, svydsgn, na.rm = TRUE) 
+svydsgn |>
+  summarise(estimacion = survey_total(b6_1, vartype = c("cv","se"), 
+                                      na.rm = TRUE),
+            n = unweighted(n()))
 
-#total de trabajadores por región (validado)
-svyby(~b6_1, ~region, svydsgn, svytotal, na.rm = TRUE, vartype= c("se", "cv")) |>
-  mutate(`Distribución %` = b6_1/sum(b6_1) *100,
-         `N° trabajadores` = b6_1,
-         `Región` = region) |>
-  arrange(desc(`Distribución %`)) |>
-  select(`Región`, `N° trabajadores`, `Distribución %`, se, cv)
-  
-#total de trabajadores por cod_CIIU_seccion (por validar)*
-svyby(~b6_1, ~a1_cod_CIIU_seccion, svydsgn, svytotal, na.rm = TRUE, vartype= c("se", "cv")) |>
-  mutate(`Distribución %` = b6_1/sum(b6_1) *100,
-         `N° trabajadores` = b6_1,
-         `CIIU4.CL` = a1_cod_CIIU_seccion) |>
-  arrange(`CIIU4.CL`) |>
-  select(`CIIU4.CL`, `N° trabajadores`, `Distribución %`, se, cv)
+# Distribución de empresas por región
+nro_empresas <- svydsgn |>
+  group_by(region) |>
+  summarise(`N° empresas` = survey_total(vartype = "cv"),
+            `Tamaño muestral` = unweighted(n())) |>
+  mutate(`Distribución %` = `N° empresas`/sum(`N° empresas`) *100,
+           Región = region) |>
+  arrange(desc(`N° empresas`)) |>
+  select(Región, `Tamaño muestral`, `N° empresas`, `Distribución %`)
+
+total_pais <- 
+nro_empresas |>
+  summarise(`Región` = 0,
+            `Tamaño muestral` = sum(`Tamaño muestral`),
+            `N° empresas`= sum(`N° empresas`),
+            `Distribución %` = sum(`Distribución %`))
+
+
+# Unir tabla principal con totales
+nro_empresas_final <- bind_rows(nro_empresas, total_pais)
+nro_empresas_final$Región[17] <- c("Total")
+
+knitr::kable(nro_empresas_final, 
+             digits=1,
+             align=rep('c', 5),
+             format.args = list(decimal.mark = ',', big.mark = "."))
+
+rm(total_pais, nro_empresas)
+
+#total de trabajadores por región
+
+nro_trabajadores <- 
+svydsgn |>
+  group_by(region) |>
+  summarise(`N° trabajadores` = survey_total(b6_1, vartype = "cv"),
+            `Tamaño muestral` = unweighted(n())) |>
+  mutate(`Distribución %` = `N° trabajadores`/sum(`N° trabajadores`) *100,
+         Región = region) |>
+  arrange(desc(`N° trabajadores`)) |>
+  select(Región, `Tamaño muestral`, `N° trabajadores`, `Distribución %`)
+
+total_pais <- 
+  nro_trabajadores |>
+  summarise(`Región` = 0,
+            `Tamaño muestral` = sum(`Tamaño muestral`),
+            `N° trabajadores`= sum(`N° trabajadores`),
+            `Distribución %` = sum(`Distribución %`))
+
+# Unir tabla principal con totales
+nro_trabajadores_final <- bind_rows(nro_trabajadores, total_pais)
+nro_trabajadores_final$Región[17] <- c("Total")
+
+knitr::kable(nro_trabajadores_final, 
+             digits=1,
+             align=rep('c', 5),
+             format.args = list(decimal.mark = ',', big.mark = "."))
+
+rm(total_pais, nro_trabajadores)
+
+#total de trabajadores por cod_CIIU_seccion
 
 # Distribución de empresas (E) y trabajadores (T) por región
 
